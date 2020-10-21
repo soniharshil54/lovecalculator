38902df480b00f9065529981817a9d22ff3c2822
soniharshil54 <sharshil43@yahoo.com>
2020-10-21 23:14:49 +0530

    first commit

---
+++ dist/scripts/love.js
@@ -0,0 +1,36 @@
+  function calculateLove(){
+    console.log("calculate")
+    let firstName = document.getElementById("name1").value
+    let lastName = document.getElementById("name2").value
+    fetch(`https://rapidapi.p.rapidapi.com/getPercentage?fname=${firstName}&sname=${lastName}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "3175010eeemsh93249d3cef8d24ap1c6a65jsnaca52626d09e"
+      }
+    })
+    .then(response => {
+      console.log("response")
+      return response.json();
+      // document.getElementById("resultlove").value = response
+    })
+    .then(res => {
+      console.log("res")
+      console.log(res)
+      document.getElementById("resultlove").innerHTML = `${res.percentage}%`
+      document.getElementById("resultloveres").innerHTML = res.result
+
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+$(document).ready(function() {
+  $("#formlove").submit(function(event){
+    if(true) { 
+       event.preventDefault();  
+    }  
+    calculateLove()
+  });
+});
